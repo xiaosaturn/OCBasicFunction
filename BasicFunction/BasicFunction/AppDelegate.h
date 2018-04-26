f9777a36d26e8c28055fe0a57fac480e8e493844
晓锟 <anker.huang@scysun.com>
2018-04-26 11:00:57 +0800

    init

---
+++ BasicFunction/BasicFunction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicFunction
+//
+//  Created by 斯盛123 on 2018/4/26.
+//  Copyright © 2018年 Anker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicFunction/BasicFunction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicFunction
+//
+//  Created by 斯盛123 on 2018/4/26.
+//  Copyright © 2018年 Anker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

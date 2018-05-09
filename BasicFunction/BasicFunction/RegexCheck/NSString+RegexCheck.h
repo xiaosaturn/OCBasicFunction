d09bf47545389b2c937494f5057becb6ed6f1bb5
晓锟 <anker.huang@scysun.com>
2018-05-09 18:27:43 +0800

    手机号校验

---
+++ BasicFunction/BasicFunction/RegexCheck/NSString+RegexCheck.h
@@ -0,0 +1,15 @@
+//
+//  NSString+RegexCheck.h
+//  BasicFunction
+//
+//  Created by 斯盛123 on 2018/5/9.
+//  Copyright © 2018年 Anker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (RegexCheck)
+
++ (BOOL)phoneIsEffectiveMobile:(NSString *)mobile;
+
+@end
